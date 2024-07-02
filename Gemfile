18787690165bf9fd14d6ce2d10871b19e7e7c389
pou0119 <yuuyiteng0@gmail.com>
2024-07-02 12:10:22 +0900

    answer bonus_brink.rb

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
-ruby "3.3.1"
+ruby "3.3.3"
 
 gem "rspec", "~> 3.13.0"
--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount
+    empty_bottles = amount
+
+    while empty_bottles >= 3
+      new_drinks = empty_bottles / 3
+      total += new_drinks
+      empty_bottles = empty_bottles % 3 + new_drinks
+    end
+
+    total
   end
 end
+
+# 例えば最初に100本購入した場合
+puts BonusDrink.total_count_for(100)
